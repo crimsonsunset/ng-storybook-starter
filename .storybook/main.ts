d5483131dfba6b65aab964591857d2a8386d2930
Joe Sangiorgio <jsangio1@gmail.com>
2020-03-23 19:20:56 -0400

    add docs support

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,11 @@
 module.exports = {
-  stories: ['../src/**/*.stories.[tj]s'],
+  // stories: ['../src/**/*.stories.[tj]s'],
+  stories: ['../src/**/*.stories.([tj]s|mdx)'],
   addons: [
     '@storybook/addon-actions/register',
     '@storybook/addon-links/register',
     '@storybook/addon-notes/register',
-    '@storybook/addon-storysource'
+    '@storybook/addon-storysource',
+    '@storybook/addon-docs'
   ]
 };
